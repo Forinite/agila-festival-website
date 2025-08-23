44df065fff6f5f8d0fc9b40173bc5d32cc15d28c
Forinite <obefortune2@gmail.com>
2025-08-23 14:58:44 +0100

    Cleanup

--- app/(components)/contact/page.tsx
+++ app/(components)/contact/page.tsx
@@ -11,6 +11,7 @@ const ContactSection = () => {
                     <h2 className="text-4xl md:text-5xl font-black text-black mb-4">
                         Contact <span className="text-red-500">Us</span>
                     </h2>
+                    <h5 className="md:text-md text-xs text-gray-500 max-w-3xl mx-auto">  Office address: <address>17 OGIRI OKO STREET, OTUKPO, BENUE STATE</address></h5>
                     <p className="md:text-lg text-sm text-gray-600 max-w-3xl mx-auto">
                         Get in touch with the Agila Carnival team for inquiries, partnerships, media requests, or any questions about the festival.
                     </p>
