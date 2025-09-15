45d212156101be79d587b655252692ac3061fe63
Forinite <obefortune2@gmail.com>
2025-09-15 17:14:23 +0100

    Issue fixing MileStone. Next add blob logic

--- app/api/get-upload-url/route.ts
+++ app/api/get-upload-url/route.ts
@@ -1,23 +1,23 @@
 // app/api/get-uploaded-url/route.ts
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getSignedUrl } from '@vercel/blob/server';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        const { filename, contentType } = req.body;
-
-        try {
-            const { url, fields } = await getSignedUrl('put', filename, {
-                contentType,
-                access: 'public',
-            });
-
-            res.status(200).json({ url, fields });
-        } catch (error) {
-            res.status(500).json({ error: 'Failed to generate signed URL' });
-        }
-    } else {
-        res.status(405).json({ error: 'Method Not Allowed' });
-    }
-}
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { getSignedUrl } from '@vercel/blob/server';
+//
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     if (req.method === 'POST') {
+//         const { filename, contentType } = req.body;
+//
+//         try {
+//             const { url, fields } = await getSignedUrl('put', filename, {
+//                 contentType,
+//                 access: 'public',
+//             });
+//
+//             res.status(200).json({ url, fields });
+//         } catch (error) {
+//             res.status(500).json({ error: 'Failed to generate signed URL' });
+//         }
+//     } else {
+//         res.status(405).json({ error: 'Method Not Allowed' });
+//     }
+// }
