5efd64254cac5bd84632a50b8b59a2c3574fd49d
Forinite <obefortune2@gmail.com>
2025-09-16 22:07:01 +0100

    Final Clean Up

--- app/components/ui/adminUI/addFeedFormModal.tsx
+++ app/components/ui/adminUI/addFeedFormModal.tsx
@@ -189,7 +189,7 @@ const AddFeedFormModal = ({ onClose, onSubmit, refetch }: AddFeedFormModalProps)
                 onSubmit={useBlob ? handleBlobSubmit : handleSubmit}
                 className="bg-white rounded-xl p-6 w-full h-fit max-w-lg text-gray-800 shadow-xl"
             >
-                <h3 className="text-xl font-bold mb-6 text-center">Add New Feed_Dev_</h3>
+                <h3 className="text-xl font-bold mb-6 text-center">Add New Feed</h3>
 
                 {/* Title */}
                 <div className="mb-4">
